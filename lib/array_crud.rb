21b7d677d0e6f997ed511010bc024b181de53c0b
Jordan Dains <dains.jordan2@gmail.com>
2020-09-08 17:41:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,8 @@ def
   ["Jordan", "Angela", "Jacob", "Ellie"]
 end
 
-def 
-  add_element_to_start_of_array(array, element)
-  array >> element
+def add_element_to_start_of_array(array, element)
+  array.push(element)
 end
 
 def 
