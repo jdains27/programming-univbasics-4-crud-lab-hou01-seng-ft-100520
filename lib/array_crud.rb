19206a308705cd9b15054d1e3e1cf7f6dfe3cbe4
Jordan Dains <dains.jordan2@gmail.com>
2020-09-08 17:25:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,18 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array 
+  []
 end
 
-def create_an_array
-  
+def 
+  create_an_array  
+  ["Jordan", "Angela", "Jacob", "Ellie"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def 
+  add_element_to_end_of_array(array, element) 
+  ["Jordan", "Angela", "Jacob", "Ellie"]
+  element = "Dallen"
+  add_element_to_end_of_array(array, element).push("Ellie")
 end
 
 def add_element_to_start_of_array(array, element)
