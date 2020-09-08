813bcf2351f1328afa160174a22b783151838c70
Jordan Dains <dains.jordan2@gmail.com>
2020-09-08 17:35:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,12 +23,14 @@ def
   array.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def 
+  remove_element_from_start_of_array(array)
+  array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def 
+  retrieve_element_from_index(array, index_number)
+  array.[index]
 end
 
 def retrieve_first_element_from_array(array)
